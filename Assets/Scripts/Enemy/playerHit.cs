5af753d458791707030e705ea9a9a3ebef8085d8
Heric Felipe de Freitas <35154816+hefefe@users.noreply.github.com>
2019-02-20 23:13:07 -0300

    hitEnemy

--- Assets/Scripts/Enemy/playerHit.cs
+++ Assets/Scripts/Enemy/playerHit.cs
@@ -2,35 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyHit : MonoBehaviour
+public class playerHit : MonoBehaviour
 {
-
     public GameObject player;
-    private Rigidbody2D rb2D;
-
-
-
+    
     public int x;
     public int y;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb2D = player.GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            rb2D.AddForce(x, y);
-            Destroy(gameObject);
+            player.transform.position = new Vector3(x, y);
         }
     }
 }
