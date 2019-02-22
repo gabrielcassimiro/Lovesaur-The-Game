833eb04b1f3d426de1095a4cf1023fbbb6d1df32
Heric Felipe de Freitas <35154816+hefefe@users.noreply.github.com>
2019-02-22 16:22:32 -0300

    Menu

---
+++ Assets/Scripts/Menu/Botoes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Botoes : MonoBehaviour
+{
+    public void btScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void btQuit()
+    {
+        Application.Quit();
+    }
+}
