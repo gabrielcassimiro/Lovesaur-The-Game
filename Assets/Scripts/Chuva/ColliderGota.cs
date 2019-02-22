2c418a3921821d21f2aef02f7dcd7a82027ffc44
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2019-02-22 16:42:53 -0300

    Chuva Funcionando

---
+++ Assets/Scripts/Chuva/ColliderGota.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderGota : MonoBehaviour
+{
+    public Transform Respawn;
+
+    private void Awake() {
+        Respawn = GameObject.Find("Respawn").GetComponent<Transform>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            collision.gameObject.transform.position = Respawn.position;
+            Destroy(this.gameObject);
+        }
+
+        if (collision.CompareTag("Line") || collision.CompareTag("Ground")) {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Chuva/SpawnGotas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGotas : MonoBehaviour
+{
+    public GameObject gota;
+    public float x1, x2, y;
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(spawn());
+        
+    }
+    IEnumerator spawn() {
+        float spawnX = Random.Range(x1, x2);
+        Instantiate(gota, new Vector3(spawnX, y, 0), Quaternion.identity);
+        yield return new WaitForSeconds(2f);
+        
+    }
+}
